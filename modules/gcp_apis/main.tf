9f8c18a6e3d70ff249f5fc456d2d994468208054
tobias-repareo <111357112+tobias-repareo@users.noreply.github.com>
2023-11-07 10:43:03 +0100

    Added cloud resource manager api

--- modules/gcp_apis/main.tf
+++ modules/gcp_apis/main.tf
@@ -3,7 +3,8 @@ resource "google_project_service" "gcp_apis" {
     "apigee.googleapis.com",
     "servicenetworking.googleapis.com",
     "compute.googleapis.com",
-    "cloudkms.googleapis.com"
+    "cloudkms.googleapis.com",
+    "cloudresourcemanager.googleapis.com"
   ])
   
   project = var.gcp_project_id
